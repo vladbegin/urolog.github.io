f842562cb8d25237bcad0f0ea469f46302fe783d
Vladimir Pilyuganov <48057253+vladbegin@users.noreply.github.com>
2022-08-17 22:04:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,14 +15,7 @@ let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
 
 btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
-		item = "1";
-		tg.MainButton.show();
-	}
+	tg.openLink('https://vladbegin.github.io/urolog.github.io/leiko.html');
 });
 
 btn2.addEventListener("click", function(){
